656948212190e224dff23c76ab1d5d7218fa9356
anggitakakom <anggit_new@ymail.com>
2018-12-19 13:46:09 +0700

    push simple tampil

---
+++ 1_example.php
@@ -0,0 +1,15 @@
+<?php
+//penggunaan echo dan echo () akan menampilkan hal yang sama
+echo"nama echo"."</br>";
+echo("nama echo ()"."</br>");
+
+//penggunaan print dan print () akan menampilkan hal yang sama
+print"nama print"."</br>";
+print("nama print ()");
+
+// hasil dari keseluruhan :
+// echo dan print sama sama untuk menampilkan data yang berdada dalam php
+// dan lebih enak menggunakan print() atau echo() karena lebih enak
+// dan sesuai dengan bahasa pemograman yang lain
+
+?>
